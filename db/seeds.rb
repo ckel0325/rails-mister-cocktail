86c0885f49135f4de765b3478c3cec2ac2eb8ba1
Kelvin Chow <c.kelvin.k@gmail.com>
2018-07-27 16:44:30 +0800

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,21 @@
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+require 'open-uri'
+require 'json'
+
+Ingredient.delete_all
+puts "Deleted all ingredients"
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+cocktail = open(url).read
+results = JSON.parse(cocktail)
+
+ingredients = results["drinks"].map do |result|
+  result["strIngredient1"]
+end
+
+ingredients.sort!
+
+ingredients.each do |ingredient|
+  Ingredient.create!(name: ingredient)
+end
+
+puts "created #{Ingredient.count} ingredients!"
